6c3388905f37a2170d6e9d28914abfde25857c0a
tpaskhalis <tpaskhalis@gmail.com>
2014-05-11 14:23:13 +0100

    Updated text analysis script

--- analysis.py
+++ analysis.py
@@ -10,3 +10,15 @@ inlinfo.to_csv('OEIL/inl_info.csv', index = False)
 
 inl = pd.read_table('OEIL/text/INL_2007-2238.tsv', names = ['num', 'par'], sep = '\t')
 lpar = inl['par'].tolist()
+tokens = []
+for i in lpar:
+    tokens.extend(nltk.word_tokenize(i))
+fdist = nltk.FreqDist(tokens)
+for word in fdist:
+    print word, '->', fdist[word], ';',
+
+bigram_measures = nltk.collocations.BigramAssocMeasures()
+finder = nltk.BigramCollocationFinder.from_words(tokens)
+finder.nbest(bigram_measures.pmi, 5)
+
+scored = finder.score_ngrams(bigram_measures.raw_freq)
